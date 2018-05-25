ab4bce10bbd1f38c5666d64fd02724deceb05389
CR351 <22698761+CR351@users.noreply.github.com>
2018-05-25 12:33:44 -0400

    Update resnet50.py

--- keras/applications/resnet50.py
+++ keras/applications/resnet50.py
@@ -210,8 +210,7 @@ def ResNet50(include_top=True, weights='imagenet',
     else:
         bn_axis = 1
 
-    x = ZeroPadding2D(padding=(3, 3), name='conv1_pad')(img_input)
-    x = Conv2D(64, (7, 7), strides=(2, 2), padding='valid', name='conv1')(x)
+    x = Conv2D(64, (7, 7), strides=(2, 2), padding='same', name='conv1')(img_input)
     x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
     x = Activation('relu')(x)
     x = MaxPooling2D((3, 3), strides=(2, 2), padding = 'same')(x)
